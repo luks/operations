4d0aad321c1c5997111979e206528dc17079e077
Luky <lukepiske@gmail.com>
2013-06-17 07:54:39 +0200

    Some staff belongs to model removed from controller to model

--- app/controllers/datacenters_controller.rb
+++ app/controllers/datacenters_controller.rb
@@ -192,60 +192,14 @@ class DatacentersController < ApplicationController
     #update
 
     if(params[:day_collections])
-      DayCollection.transaction do
-        params[:day_collections].map do |k,r|
-          if(users.include? r[:user_id])
-            sql_u = ActiveRecord::Base.send(:sanitize_sql_array,
-              ["UPDATE day_collections SET status_id = #{r[:status_id]},shift_id = #{r[:shift_id]},center_id = #{r[:center_id]}
-              WHERE day_collections.id = #{r[:id]}"]
-            )
-                Thread.new do
-                  day_collection = DayCollection.find(r[:id])
-                  fake = DayCollection.new(r)
-                  Notifier.admin_update_shift(day_collection, fake).deliver unless fake.identical? day_collection
-                  ActiveRecord::Base.connection.close
-                end
-                DayCollection.connection.execute(sql_u)
-          else
-            sql_d = ActiveRecord::Base.send(:sanitize_sql_array,
-              ["DELETE FROM day_collections WHERE day_collections.id = #{r[:id]}"]
-            )
-                Thread.new do
-                  Notifier.admin_delete_shift(DayCollection.find(r[:id])).deliver
-                  ActiveRecord::Base.connection.close
-                end
-            DayCollection.connection.execute(sql_d)
-
-          end
-        end
-      end
+      DayCollection.update_fast(users,params[:day_collections])
     end
 
     #insert
 
     if(params[:day_collections_new])
-      DayCollection.transaction do
-        @collections = []
-        params[:day_collections_new].map do |r|
-          if(users.include? r[:user_id])
-            sql = ActiveRecord::Base.send(:sanitize_sql_array,
-             ["INSERT INTO day_collections
-             (day_id, shift_id, status_id, user_id, center_id) VALUES
-             (#{r[:day_id]}, #{r[:shift_id]}, #{r[:status_id]},#{r[:user_id]},#{r[:center_id]})"]
-            )
-            DayCollection.connection.execute(sql)
-            @collections << DayCollection.where(:day_id => r[:day_id],:user_id => r[:user_id]).first 
-          end
-        end
-      end
-        
-        Thread.new do  
-          @collections.each do |collection|        
-            Notifier.admin_create_shift(collection).deliver
-            ActiveRecord::Base.connection.close
-          end
-        end
-      end
+      DayCollection.create_fast(users,params[:day_collections_new])
+    end
     
     respond_to do |format|
         format.html {  redirect_to datacenter_url(date_to_params(day.date)) }
--- app/models/day_collection.rb
+++ app/models/day_collection.rb
@@ -13,17 +13,58 @@ class DayCollection < ActiveRecord::Base
                       WHERE  (days.date BETWEEN '#{from}' AND '#{to}')")
   end
 
-  def self.update_fast(r)
-    self.connection.execute(sanitize_sql(
+  def self.update_fast(users,params)
+    puts params
+      self.transaction do
+        params.map do |k,r|
+          if(users.include? r[:user_id])
+            sql_u = ActiveRecord::Base.send(:sanitize_sql_array,
               ["UPDATE day_collections SET status_id = #{r[:status_id]},shift_id = #{r[:shift_id]},center_id = #{r[:center_id]}
               WHERE day_collections.id = #{r[:id]}"]
-            ))
+            )
+                Thread.new do
+                  day_collection = DayCollection.find(r[:id])
+                  fake = DayCollection.new(r)
+                  Notifier.admin_update_shift(day_collection, fake).deliver unless fake.identical? day_collection
+                  ActiveRecord::Base.connection.close
+                end
+                DayCollection.connection.execute(sql_u)
+          else
+            sql_d = ActiveRecord::Base.send(:sanitize_sql_array,
+              ["DELETE FROM day_collections WHERE day_collections.id = #{r[:id]}"]
+            )
+                Thread.new do
+                  Notifier.admin_delete_shift(DayCollection.find(r[:id])).deliver
+                  ActiveRecord::Base.connection.close
+                end
+            DayCollection.connection.execute(sql_d)
+
+          end
+        end
+      end
   end
 
-  def self.create_fast(r)
-    self.connection.execute(sanitize_sql(["INSERT INTO day_collections
+  def self.create_fast(users,params)
+      self.transaction do
+        @collections = []
+        params.map do |r|
+          if(users.include? r[:user_id])
+            sql = ActiveRecord::Base.send(:sanitize_sql_array,
+             ["INSERT INTO day_collections
              (day_id, shift_id, status_id, user_id, center_id) VALUES
-             (#{r[:day_id]}, #{r[:shift_id]}, #{r[:status_id]},#{r[:user_id]},#{r[:center_id]})"]))
+             (#{r[:day_id]}, #{r[:shift_id]}, #{r[:status_id]},#{r[:user_id]},#{r[:center_id]})"]
+            )
+            DayCollection.connection.execute(sql)
+            @collections << DayCollection.where(:day_id => r[:day_id],:user_id => r[:user_id]).first 
+          end
+        end
+      end  
+        Thread.new do  
+          @collections.each do |collection|        
+            Notifier.admin_create_shift(collection).deliver
+            ActiveRecord::Base.connection.close
+          end
+        end
   end  
 
   def self.delete_fast(r)
